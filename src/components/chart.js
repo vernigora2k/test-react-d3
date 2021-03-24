b10e57c88b7fadb9ba2de49d662658c64ebf9d24
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2021-03-24 09:35:11 +0200

    experiments with d3 chart

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,10 +1,42 @@
 import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
 import './styles/chart.css';
 
-const Chart = () => {
+const Chart = ({xdim, ydim, margin, xdata, ydata}) => {
+
+    const canvas = useRef(null)
+
+    useEffect(() => {
+        const svg = d3.select(canvas.current)
+
+        addAxes(svg)
+    }, [xdim, ydim, margin, xdata, ydata])
+
+    const addAxes = (svg) => {
+        const xscale = d3.scaleBand()
+            .domain(xdata)
+            .range([margin.left, xdim + margin.left])
+
+        const xAxis = d3.axisBottom(xscale)
+
+        svg.append('g')
+            .call(xAxis)
+            // .attr('transform', `translate(0, ${ydim})`)
+            .attr('transform', `translate(0, ${ydim - 25})`)
+    }
+
     return (
         <div className='canvas'>
-            <h1>chart, this is chart canvas</h1>
+            <svg
+                viewBox={`0 0 ${xdim + margin.left + margin.right} ${ydim + margin.top }`}
+                preserveAspectRatio='xMinYMin'
+                width='100%'
+                height='100%'
+                style={{backgroundColor: 'beige'}}
+                ref={canvas}
+            >
+
+            </svg>
         </div>
     )
 }
--- src/pages/barchart.js
+++ src/pages/barchart.js
@@ -4,14 +4,26 @@ import Chart from '../components/chart';
 import './styles/barchart.css';
 
 const BarChart = () => {
-    const months = ['jun', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'august', 'sep', 'oct', 'nov', 'dec']
-    const vvls = [5, 6, 2, 7, 9, 1, 2, 3, 8, 3, 6, 7]
+    const months = ['mar', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'august', 'sep', 'oct', 'nov', 'dec']
+    const yvalues = [5, 6, 2, 7, 9, 1, 2, 3, 8, 3, 6, 7]
 
     return (
         <div className='barchart'>
             <Header />
             <div className='barchart-body'>
-                <Chart />
+                <Chart 
+                    xdim={1440} 
+                    ydim={500} 
+                    margin={{
+                        top: 80,
+                        left: 120,
+                        right: 120,
+                        bottom: 80
+                    }} 
+                    xdata={months}
+                    ydata={yvalues}
+
+                />
             </div>
         </div>
     )
